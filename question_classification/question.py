d625dbf22def4efc0bd737dcda6a53c0ddf7470d
justindomingue <justin.domingue@hotmail.com>
2015-11-14 19:47:12 -0500

    patterns to help question head word identification

--- question_classification/question.py
+++ question_classification/question.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import re
-
+from string import punctuation
 
 class Question:
     """Question
@@ -14,12 +14,29 @@ class Question:
         type (string): wh_word of the question
 
     """
+
+    # Regex patterns to help question head word identification
+    patterns = {
+        'DESC:def': ['^what (is|are) ((a|an|the) )?(\w+ ?){1,2}', '^what (do|does) (\w+ ?)+ mean$'],
+        'ENTY:substance': ['^what (is|are) (\w+ ?)+ ((composed|made|made out) of)$'],
+        'DESC:desc': ['^what (does) (\w+ ?)+ (do)$'],
+        'ENTY:term': ['^what do you call'],
+        'DESC:reason': ['^what causes?', '^what (is|are) (\w+ ?)+ (used for)$'],
+        'ABBR:exp': ['^what (do|does) (\w+ ?)+ (stand for)$'],
+        'HUM:desc': '^who (is|was) [A-Z]'
+    }
     def __init__(self, question):
-        self.text = question
-        self.words = self.text.split(' ')
+        self.text = self.preprocess(question)
+        self.words = self.text.split(' ')   #TODO consider tokenizing
 
         wh_word = self.words[0].lower()
         self.type = wh_word if wh_word in ['what', "which", "when", "where", "who", "how", "why"] else "rest"
+
+    def preprocess(self, text):
+        """Removes punctuation and makes text lowecase"""
+        return text.translate(None, punctuation)
+
+    ### SHAPE
 
     def shape(self, text):
         """Returns the (case) shape of the question as a string"""
@@ -39,8 +56,36 @@ class Question:
 
     def word_shape(self):
         """Returns the word shape for each word in the question"""
-        return [self.shape(word) for word in self.words]
+        return [self.shape(word) for word in self.text.split(' ')]
 
+    ### HEAD WORD
+
+    def head_word(self):
+        """head_word Gets the head word of the question
+        From Huang et al. 2008)
+        """
+
+        if self.type in ['when', 'where', 'why']:
+            return None
+
+        if self.type == 'how':
+            return self.words[1]
+
+        if self.type == 'what':
+            for placehold, patterns in Question.patterns.iteritems():
+                if placehold == 'HUM:desc': continue
+
+                for pattern in patterns:
+                    if re.match(pattern, self.text):
+                        return placehold
+
+        if self.type == 'who' and re.match(Question.patterns['HUM:desc'], self.text):
+            return 'HUM:desc'
+
+        return None
+
+
+    ### OTHER
     def __repr__(self):
         return "Question({0})".format(self.text)
 
--- question_classification/tests/test_question.py
+++ question_classification/tests/test_question.py
@@ -19,3 +19,49 @@ class QuestionTests(unittest.TestCase):
         self.assertEqual(self.q1.word_shape(), ['all_upper', 'all_upper', 'all_upper'])
         self.assertEqual(self.q2.word_shape(), ['all_lower' for _ in range(len(self.q2))])
         self.assertEqual(self.q3.word_shape(), ['mixed', 'all_digit', 'other'])
+
+    def test_head_word(self):
+        #should be  None
+        self.assertEqual(Question('when is').head_word(), None)
+        self.assertEqual(Question('where is').head_word(), None)
+        self.assertEqual(Question('why is').head_word(), None)
+
+        #should be word following how
+        self.assertEqual(Question('how is').head_word(), 'is')
+
+        #test patterns
+        #DESC:def pattern 1
+        self.assertEqual(Question('what is an orange').head_word(), 'DESC:def')    #what+is+an+1word
+        self.assertEqual(Question('what are cold brewed').head_word(), 'DESC:def')  #what+are+(2 words)
+
+        #DESC:def pattern 2
+        self.assertEqual(Question('what does this thing mean?').head_word(), 'DESC:def')    #what+do/does+...+mean
+
+        #ENTY:substance
+        self.assertEqual(Question('what is a senate composed of').head_word(), 'ENTY:substance')    #what+is/are+...+composed of/made of/made out of
+        self.assertEqual(Question('what is a state senate made of').head_word(), 'ENTY:substance')
+        self.assertEqual(Question('what are senates made out of').head_word(), 'ENTY:substance')
+
+        #DESC:desc
+        self.assertEqual(Question('what does this thing do').head_word(), 'DESC:desc')  #what+does+...+do
+
+        #ENTY:term
+        self.assertEqual(Question('what do you call a ...').head_word(), 'ENTY:term')   #what+do+you+call
+
+        #DESC:reason pattern 1
+        self.assertEqual(Question('what causes a').head_word(), 'DESC:reason')  #what causes/cause
+        self.assertEqual(Question('what cause a').head_word(), 'DESC:reason')  #what causes/cause
+
+        #DESC:reason pattern 2
+        self.assertEqual(Question('what is this used for').head_word(), 'DESC:reason')     #what+is/are+...+used+for
+        self.assertEqual(Question('what are this used for').head_word(), 'DESC:reason')
+
+        #ABBR:exp
+        self.assertEqual(Question('what does this stand for').head_word(), 'ABBR:exp')  #what+does/do+...+stand+for
+
+        #should be HUM:desc
+        self.assertEqual(Question('who is John...').head_word(), 'HUM:desc')    #what+is/was+(Capital letter)
+        self.assertEqual(Question('who was Carla...').head_word(), 'HUM:desc')
+
+        #should be candidate head word using head finder rules
+        self.assertEqual(Question('who is john').head_word(), None)
