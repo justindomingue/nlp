bb8846c2d25ad24d4761e4efef7f5f78d961d0fb
justindomingue <justin.domingue@hotmail.com>
2015-11-18 12:42:51 -0500

    added bblipparser module

--- question_classification/question.py
+++ question_classification/question.py
@@ -2,6 +2,9 @@
 
 import re
 from string import punctuation
+from bllipparser import RerankingParser, Tree, tokenize
+
+rrp = RerankingParser.fetch_and_load('WSJ+Gigaword-v2')
 
 class Question:
     """Question
@@ -17,24 +20,31 @@ class Question:
 
     # Regex patterns to help question head word identification
     patterns = {
-        'DESC:def': ['^what (is|are) ((a|an|the) )?(\w+ ?){1,2}', '^what (do|does) (\w+ ?)+ mean$'],
-        'ENTY:substance': ['^what (is|are) (\w+ ?)+ ((composed|made|made out) of)$'],
-        'DESC:desc': ['^what (does) (\w+ ?)+ (do)$'],
+        'DESC:def': ['^what (is|are) ((a|an|the) )?(\w+ ?){1,2}', '^what (do|does) (\w+ ?)+ mean ?$'],
+        'ENTY:substance': ['^what (is|are) (\w+ ?)+ ((composed|made|made out) of) ?$'],
+        'DESC:desc': ['^what (does) (\w+ ?)+ (do) ?$'],
         'ENTY:term': ['^what do you call'],
-        'DESC:reason': ['^what causes?', '^what (is|are) (\w+ ?)+ (used for)$'],
-        'ABBR:exp': ['^what (do|does) (\w+ ?)+ (stand for)$'],
+        'DESC:reason': ['^what causes?', '^what (is|are) (\w+ ?)+ (used for) ?$'],
+        'ABBR:exp': ['^what (do|does) (\w+ ?)+ (stand for) ?$'],
         'HUM:desc': '^who (is|was) [A-Z]'
     }
+
     def __init__(self, question):
+        """
+        Creates a Question
+        :param question: question text. Must be ended with a ' ?'
+        """
+
         self.text = self.preprocess(question)
-        self.words = self.text.split(' ')   #TODO consider tokenizing
+        self.words = tokenize(self.text)
 
         wh_word = self.words[0].lower()
         self.type = wh_word if wh_word in ['what', "which", "when", "where", "who", "how", "why"] else "rest"
 
     def preprocess(self, text):
         """Removes punctuation and makes text lowecase"""
-        return text.translate(None, punctuation)
+        # return text.translate(None, punctuation)
+        return text #TODO get rid of it?
 
     ### SHAPE
 
@@ -54,12 +64,14 @@ class Question:
             else:
                 return 'other'
 
+    @property
     def word_shape(self):
         """Returns the word shape for each word in the question"""
         return [self.shape(word) for word in self.text.split(' ')]
 
     ### HEAD WORD
 
+    @property
     def head_word(self):
         """head_word Gets the head word of the question
         From Huang et al. 2008)
@@ -84,6 +96,15 @@ class Question:
 
         return None
 
+    def apply_collins_rules_on(self, tree):
+        return
+
+    def extract_head_word(self):
+        sentence = self.words + ['?']               #add '?' so parser knows it's a question (parse differs otherwise)
+        tree = Tree(rrp.simple_parse(sentence))     #get the best parse
+
+        return self.apply_collins_rules_on(tree)
+
 
     ### OTHER
     def __repr__(self):
@@ -93,3 +114,32 @@ class Question:
         return len(self.words)
 
 
+
+
+def load_instances(f):
+    '''
+    Load two lists of cases to perform WSD on. The structure that is returned is a dict, where
+    the keys are the ids, and the values are instances of WSDInstance.
+    '''
+    tree = ET.parse(f)
+    root = tree.getroot()
+
+    dev_instances = {}
+    test_instances = {}
+
+    for text in root:
+        if text.attrib['id'].startswith('d001'):
+            instances = dev_instances
+        else:
+            instances = test_instances
+        for sentence in text:
+            # construct sentence context
+            context = [to_ascii(el.attrib['lemma']) for el in sentence]
+            for i, el in enumerate(sentence):
+                if el.tag == 'instance':
+                    my_id = el.attrib['id']
+                    lemma = to_ascii(el.attrib['lemma'])
+                    instances[my_id] = WSDInstance(my_id, lemma, context, i)
+    return dev_instances, test_instances
+
+if __name__ == "__main__":
--- question_classification/tests/test_question.py
+++ question_classification/tests/test_question.py
@@ -6,14 +6,14 @@ from question_classification.question import Question
 
 class QuestionTests(unittest.TestCase):
     def setUp(self):
-        self.q1 = Question('WHERE IS CANADA')
-        self.q2 = Question('when can you find this')
-        self.q3 = Question('Did 123 a1')
+        self.q1 = Question('WHERE IS CANADA ?')
+        self.q2 = Question('when can you find this ?')
+        self.q3 = Question('Did 123 a1 ?')
 
     def test_init(self):
-        self.assertEqual(self.q1.type, 'where')
-        self.assertEqual(self.q2.type, 'when')
-        self.assertEqual(self.q3.type, 'rest')
+        self.assertEqual(self.q1.type, 'where ?')
+        self.assertEqual(self.q2.type, 'when ?')
+        self.assertEqual(self.q3.type, 'rest ?')
 
     def test_shape(self):
         self.assertEqual(self.q1.word_shape(), ['all_upper', 'all_upper', 'all_upper'])
@@ -21,48 +21,55 @@ class QuestionTests(unittest.TestCase):
         self.assertEqual(self.q3.word_shape(), ['mixed', 'all_digit', 'other'])
 
     def test_head_word(self):
+        # == FROM REGULAR EXPRESSION ==
         #should be  None
-        self.assertEqual(Question('when is').head_word(), None)
-        self.assertEqual(Question('where is').head_word(), None)
-        self.assertEqual(Question('why is').head_word(), None)
+        self.assertEqual(Question('when is ?').head_word(), None)
+        self.assertEqual(Question('where is ?').head_word(), None)
+        self.assertEqual(Question('why is ?').head_word(), None)
 
         #should be word following how
-        self.assertEqual(Question('how is').head_word(), 'is')
-        self.assertEqual(Question('how long is a race').head_word(), 'long')
+        self.assertEqual(Question('how is ?').head_word(), 'is')
+        self.assertEqual(Question('how long is a race ?').head_word(), 'long')
 
         #test patterns
         #DESC:def pattern 1
-        self.assertEqual(Question('what is an orange').head_word(), 'DESC:def')    #what+is+an+1word
-        self.assertEqual(Question('what are cold brewed').head_word(), 'DESC:def')  #what+are+(2 words)
+        self.assertEqual(Question('what is an orange ?').head_word(), 'DESC:def')    #what+is+an+1word
+        self.assertEqual(Question('what are cold brewed ?').head_word(), 'DESC:def')  #what+are+(2 words)
 
         #DESC:def pattern 2
-        self.assertEqual(Question('what does this thing mean?').head_word(), 'DESC:def')    #what+do/does+...+mean
+        self.assertEqual(Question('what does this thing mean ?').head_word(), 'DESC:def')    #what+do/does+...+mean
 
         #ENTY:substance
-        self.assertEqual(Question('what is a senate composed of').head_word(), 'ENTY:substance')    #what+is/are+...+composed of/made of/made out of
-        self.assertEqual(Question('what is a state senate made of').head_word(), 'ENTY:substance')
-        self.assertEqual(Question('what are senates made out of').head_word(), 'ENTY:substance')
+        self.assertEqual(Question('what is a senate composed of ?').head_word(), 'ENTY:substance')    #what+is/are+...+composed of/made of/made out of
+        self.assertEqual(Question('what is a state senate made of ?').head_word(), 'ENTY:substance')
+        self.assertEqual(Question('what are senates made out of ?').head_word(), 'ENTY:substance')
 
         #DESC:desc
-        self.assertEqual(Question('what does this thing do').head_word(), 'DESC:desc')  #what+does+...+do
+        self.assertEqual(Question('what does this thing do ?').head_word(), 'DESC:desc')  #what+does+...+do
 
         #ENTY:term
-        self.assertEqual(Question('what do you call a ...').head_word(), 'ENTY:term')   #what+do+you+call
+        self.assertEqual(Question('what do you call a group of turkeys ?').head_word(), 'ENTY:term')   #what+do+you+call
 
         #DESC:reason pattern 1
-        self.assertEqual(Question('what causes a').head_word(), 'DESC:reason')  #what causes/cause
-        self.assertEqual(Question('what cause a').head_word(), 'DESC:reason')  #what causes/cause
+        self.assertEqual(Question('what causes a ?').head_word(), 'DESC:reason')  #what causes/cause
+        self.assertEqual(Question('what cause a ?').head_word(), 'DESC:reason')  #what causes/cause
 
         #DESC:reason pattern 2
-        self.assertEqual(Question('what is this used for').head_word(), 'DESC:reason')     #what+is/are+...+used+for
-        self.assertEqual(Question('what are this used for').head_word(), 'DESC:reason')
+        self.assertEqual(Question('what is this used for ?').head_word(), 'DESC:reason')     #what+is/are+...+used+for
+        self.assertEqual(Question('what are this used for ?').head_word(), 'DESC:reason')
 
         #ABBR:exp
-        self.assertEqual(Question('what does this stand for').head_word(), 'ABBR:exp')  #what+does/do+...+stand+for
+        self.assertEqual(Question('what does this stand for ?').head_word(), 'ABBR:exp')  #what+does/do+...+stand+for
 
         #should be HUM:desc
-        self.assertEqual(Question('who is John...').head_word(), 'HUM:desc')    #what+is/was+(Capital letter)
-        self.assertEqual(Question('who was Carla...').head_word(), 'HUM:desc')
+        self.assertEqual(Question('who is John ?').head_word(), 'HUM:desc')    #what+is/was+(Capital letter)
+        self.assertEqual(Question('who was Carla ?').head_word(), 'HUM:desc')
 
         #should be candidate head word using head finder rules
-        self.assertEqual(Question('who is john').head_word(), None)
+        self.assertEqual(Question('who is john ?').head_word(), None)
+
+        # == FROM SYNTACTIC PARSE + COLLINS RULES ==
+        #should be 'turkeys'
+        self.assertEqual(Question('what is a group of turkeys called ?'), 'turkeys')
+        self.assertEqual(Question('what year did the titanic sink ?'), 'year')
+        self.assertEqual(Question('what is the sales tax in Minnesota ?'), 'tax')
