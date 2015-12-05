cb5bd45a2623d3bed3ca7b257162f35e25574451
Justin Domingue <justin.domingue@hotmail.com>
2015-12-05 09:41:26 -0500

    added partial semantic head finder

--- semantics/head_finder.py
+++ semantics/head_finder.py
@@ -4,9 +4,7 @@
 from bllipparser import Tree
 
 class AbstractCollinsHeadFinder:
-    '''
-    A base class for a HeadFinder similar to the one described in Michael Collins' 1999 thesis
-    '''
+    ''' A base class for a HeadFinder similar to the one described in Michael Collins' 1999 thesis '''
 
     WILDCARD = '**'
 
@@ -14,7 +12,11 @@ class AbstractCollinsHeadFinder:
         '''Determines which daugther of `tree` is the head'''
         raise NotImplementedError
 
-    def search(self, candidates, direction, tags, one_at_a_time=False):
+    def _apply_collins(self, tree, candidates):
+        '''Applies Collins' rules to find the most likely head of tree'''
+        raise NotImplementedError
+
+    def _search(self, candidates, direction, tags, one_at_a_time=False):
         """Searches `tree` in `direction` for node with label in `tags`
 
         :param tree: Tree in which to perform the search
@@ -41,19 +43,34 @@ class AbstractCollinsHeadFinder:
 
         return None
 
-    def last_word(self, tree):
+    def _last_word(self, tree):
         '''Returns the right-most tree in `tree`'''
         while tree.subtrees() != []:
             tree = tree.subtrees()[-1]
         return tree
 
-    def firstNN(self, tree, tag="NN"):
+    def _find(self, tree):
+        # tree has no children, it is its own head
+        if tree.is_preterminal():
+            return (tree.token, tree.label)
+
+        # recursively find the candidate heads of the children
+        candidate_heads = []
+        for subtree in tree.subtrees():
+            candidate_heads.append(self._find(subtree))
+
+        # now choose a head using Collins' rules
+        candidate = self._apply_collins(tree, candidate_heads)
+
+        return candidate
+
+    def _firstNN(self, tree, tag="NN"):
         '''Returns the first word whose tag starts with 'NN'''''
         try:
             return tree.tokens()[next(i for i,tag in enumerate(tree.tags()) if tag.startswith("NN"))]
         except StopIteration:
             if tag != "JJ":
-                return self.firstNN(tree, "JJ")
+                return self._firstNN(tree, "JJ")
             else:
                 return None
 
@@ -97,25 +114,6 @@ class CollinsHeadFinder(AbstractCollinsHeadFinder):
 
         # Find a candidate head  for the tree
         candidate, tag = self._find(tree)
-
-        if candidate is not None:
-            return candidate
-        else:
-            return self.firstNN(tree)
-
-    def _find(self, tree):
-        # tree has no children, it is its own head
-        if tree.is_preterminal():
-            return (tree.token, tree.label)
-
-        # recursively find the candidate heads of the children
-        candidate_heads = []
-        for subtree in tree.subtrees():
-            candidate_heads.append(self._find(subtree))
-
-        # now choose a head using Collins' rules
-        candidate = self._apply_collins(tree, candidate_heads)
-
         return candidate
 
     def _apply_collins(self, tree, candidates):
@@ -125,19 +123,19 @@ class CollinsHeadFinder(AbstractCollinsHeadFinder):
             if last_word.label == 'POS':  # last word is tag POS
                 return last_word
 
-            candidate = self.search(candidates, 'right', ['NN', 'NNP', 'NNPS', 'NNS', 'NX', 'POS', 'JJR'])
+            candidate = self._search(candidates, 'right', ['NN', 'NNP', 'NNPS', 'NNS', 'NX', 'POS', 'JJR'])
             if candidate is not None: return (candidate, tree.label)
 
-            candidate = self.search(candidates, 'left', ['NP'])
+            candidate = self._search(candidates, 'left', ['NP'])
             if candidate is not None: return (candidate, tree.label)
 
-            candidate = self.search(candidates, 'right', ['$', 'ADJP', 'PRN'])
+            candidate = self._search(candidates, 'right', ['$', 'ADJP', 'PRN'])
             if candidate is not None: return (candidate, tree.label)
 
-            candidate = self.search(candidates, 'right', ['CD'])
+            candidate = self._search(candidates, 'right', ['CD'])
             if candidate is not None: return (candidate, tree.label)
 
-            candidate = self.search(candidates, 'right', ['JJ', 'JJS', 'RB', 'QP'])
+            candidate = self._search(candidates, 'right', ['JJ', 'JJS', 'RB', 'QP'])
             if candidate is not None: return (candidate, tree.label)
 
             return (last_word, tree.label)
@@ -146,5 +144,82 @@ class CollinsHeadFinder(AbstractCollinsHeadFinder):
             raise NotImplementedError
 
         rule = CollinsHeadFinder.collins_rules[tree.label]
-        candidate = self.search(candidates, rule[0], rule[1], one_at_a_time=True)
+        candidate = self._search(candidates, rule[0], rule[1], one_at_a_time=True)
+        return (candidate, tree.label)
+
+class SemanticHeadFinder(AbstractCollinsHeadFinder):
+    '''Variant of a HeadFinder described in Collins' 1999 thesis that returns the semantic head noun of a phrase'''
+
+    # Modification of Collins' rules giving preference to noun or noun phrases
+    semantic_rules = {
+        'ADJP'  : ('left',  ['NNS','QP','NN','$','ADVP','JJ','VBN','ADJP','JJR','NP','JJS','DT','FW','RBR','RBS','SBAR','RB']),
+        'ADVP'  : ('right', ['RB','RBR','RBS','FW','ADVP','TO','CD','JJR','JJ','IN','NP','JJS','NN']),
+        'CONJP' : ('right', ['CC','RB','IN']),
+        'FRAG'  : ('right', [AbstractCollinsHeadFinder.WILDCARD]),
+        'INTJ'  : ('left',  [AbstractCollinsHeadFinder.WILDCARD]),
+        'LST'   : ('right', ['LS',':']),
+        'NAC'   : ('left',  ['NN','NNS','NNP','NNPS','NP','NAC','EX','$','CD','QP','PRP','VBG','JJ','JJS','JJR','ADJP','FW']),
+        'PP'    : ('right', ['IN','TO','VBG','VBN','RP','FW']),
+        'PRN'   : ('left',  [AbstractCollinsHeadFinder.WILDCARD]),
+        'PRT'   : ('right', ['RP']),
+        'QP'    : ('left',  ['$','IN','NNS','NN','JJ','RB','DT','CD','NCD','QP','JJR','JJS']),
+        'RRC'   : ('right', ['VP','NP','ADVP','ADJP','PP']),
+        'S1'    : ('left',  [AbstractCollinsHeadFinder.WILDCARD]),   #custom: `S1` is root of bllipparser
+        'S'     : ('left',  ['TO','IN','VP','S','SBAR','ADJP','UCP','NP']),
+        'SBAR'  : ('left',  ['WHNP','WHPP','WHADVP','WHADJP','IN','DT','S','SQ','SINV','SBAR','FRAG']),
+        'SBARQ' : ('left',  ['SQ','S','SINV','SBARQ','FRAG']),
+        'SINV'  : ('left',  ['VBZ','VBD','VBP','VB','MD','VP','S','SINV','ADJP','NP']),
+        'SQ'    : ('left',  ['VBZ','VBD','VB','MD','VP','SQ']),
+        'UCP'   : ('right', [AbstractCollinsHeadFinder.WILDCARD]),
+        'VP'    : ('left',  ['TO','VBD','VBN','MD','VBZ','VB','VBG','VBP','VP','ADJP','NN','NNS','NP']),
+        'WHADJP': ('left',  ['CC','WRB','JJ','ADJP']),
+        'WHADVP': ('right', ['CC','WRB']),
+        'WHNP'  : ('left',  ['WDT','WP','WP$','WHADJP','WHPP','WHNP']),
+        'WHPP'  : ('right', ['IN','TO','FW'])
+    }
+
+    def determine_head(self, tree):
+        '''
+        Determines which daugther of `tree` is the head
+
+        The method works by assigning a head to every node in a bottom-up fashon
+        '''
+
+        # Find a candidate head  for the tree
+        candidate, tag = self._find(tree)
+
+        if candidate is not None:
+            return candidate
+        else:
+            return self._firstNN(tree)
+
+    def _apply_collins(self, tree, candidates):
+        '''Applies Collins' rules to find the most likely head of tree'''
+        if tree.label == 'NP':
+            last_word = tree.subtrees()[-1]
+            if last_word.label == 'POS':  # last word is tag POS
+                return last_word
+
+            candidate = self._search(candidates, 'right', ['NN', 'NNP', 'NNPS', 'NNS', 'NX', 'POS', 'JJR'])
+            if candidate is not None: return (candidate, tree.label)
+
+            candidate = self._search(candidates, 'left', ['NP'])
+            if candidate is not None: return (candidate, tree.label)
+
+            candidate = self._search(candidates, 'right', ['$', 'ADJP', 'PRN'])
+            if candidate is not None: return (candidate, tree.label)
+
+            candidate = self._search(candidates, 'right', ['CD'])
+            if candidate is not None: return (candidate, tree.label)
+
+            candidate = self._search(candidates, 'right', ['JJ', 'JJS', 'RB', 'QP'])
+            if candidate is not None: return (candidate, tree.label)
+
+            return (last_word, tree.label)
+
+        elif tree.label == 'CC':
+            raise NotImplementedError
+
+        rule = CollinsHeadFinder.collins_rules[tree.label]
+        candidate = self._search(candidates, rule[0], rule[1], one_at_a_time=True)
         return (candidate, tree.label)
--- semantics/test_head_finder.py
+++ semantics/test_head_finder.py
@@ -4,12 +4,11 @@
 import unittest
 
 from bllipparser import Tree
-from head_finder import CollinsHeadFinder
+from head_finder import CollinsHeadFinder, SemanticHeadFinder
 
 
 class TestCollinsHeadFinder(unittest.TestCase):
     def test_determine_head(self):
-
         trees = [
             "(S1 (S (WHNP (WP What) (NN year)) (VP (VBD did) (S (NP (DT the) (NNP Titanic)) (VP (VB sink))))))",
             "(S1 (SBARQ (WHADVP (WRB How) (RB far)) (SQ (VBZ is) (NP (PRP it)) (PP (PP (IN from) (NP (NNP Denver))) (PP (TO to) (NP (NNP Aspen))))) (. ?)))",
@@ -32,3 +31,26 @@ class TestCollinsHeadFinder(unittest.TestCase):
         for i in range(len(trees)):
             self.assertEqual(heads[i], chf.determine_head(Tree(trees[i])))
 
+class TestSemanticHeadFinder(unittest.TestCase):
+    def test_determine_head(self):
+        trees = [
+            "(S1 (S (WHNP (WP What) (NN year)) (VP (VBD did) (S (NP (DT the) (NNP Titanic)) (VP (VB sink))))))",
+            "(S1 (SBARQ (WHADVP (WRB How) (RB far)) (SQ (VBZ is) (NP (PRP it)) (PP (PP (IN from) (NP (NNP Denver))) (PP (TO to) (NP (NNP Aspen))))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WP What) (NN county)) (SQ (VBZ is) (NP (NP (NNP Modesto)) (, ,) (NP (NNP California))) (ADVP (RB in))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WP Who)) (SQ (VP (VBD was) (NP (NNP Galileo)))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WP What)) (SQ (VBZ is) (NP (DT an) (NN atom))) (. ?)))",
+            "(S1 (SBARQ (WHADVP (WRB When)) (SQ (VBD did) (NP (NNP Hawaii)) (VP (VB become) (NP (DT a) (NN state)))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WRB How) (JJ tall)) (SQ (VP (VBZ is) (NP (DT the) (NNP Sears) (NNP Building)))) (. ?)))",
+            "(S1 (S (NP (NNP George) (NNP Bush)) (VP (VBD purchased) (NP (NP (DT a) (JJ small) (NN interest)) (PP (IN in) (WHNP (WDT which) (NN baseball) (NN team))))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WP What)) (SQ (VBZ is) (NP (NP (NNP Australia) (POS 's)) (JJ national) (NN flower))) (. ?)))",
+            "(S1 (SBARQ (WHADVP (WRB Why)) (SQ (VBZ does) (NP (DT the) (NN moon)) (VP (VB turn) (NP (NN orange)))) (. ?)))",
+            "(S1 (SBARQ (WHNP (WP What)) (SQ (VBZ is) (NP (NN autism))) (. ?)))",
+        ]
+
+        heads = ["year", "far", "country", "Galileo", "atom", "Hawaii", "Bush", "flower", "moon", "autism"]
+
+        assert len(heads) == len(trees)
+
+        chf = SemanticHeadFidner
+        for i in range(len(trees)):
+            self.assertEqual(heads[i], chf.determine_head(Tree(trees[i])))
